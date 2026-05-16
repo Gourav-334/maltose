7ca010cdbc463f1d2be3007ff2ae7d7e25157246
Gourav Kumar Mallick <12b.39gourav@gmail.com>
2026-05-16 14:02:25 +0000

    mod: Updated main program with code cleanser (parent lexer container), and updated codebase tree graph.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 /* Including required headers. */
 
 #include "../include/cmd_interp/cmd_valid.h"
+#include "../include/lexer/x86/code_cleanser.h"
+#include "../include/file_loader.h"
 
 #include <stdlib.h>
 
@@ -9,6 +11,7 @@
 int main(int argc, char *argv[])
 {
 	cmd_valid(argc, **argv, "user");
+	cleanse_code(fstream, "user");
 
 
 	return EXIT_SUCCESS;
